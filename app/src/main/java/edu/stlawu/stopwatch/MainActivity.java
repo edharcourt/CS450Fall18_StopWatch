f74cf5d86775fa3d74e3176fc8593656c9f09cde
Ed Harcourt <edharcourt@stlawu.edu>
2018-09-07 11:46:20 -0400

    .

--- app/src/main/java/edu/stlawu/stopwatch/MainActivity.java
+++ app/src/main/java/edu/stlawu/stopwatch/MainActivity.java
@@ -1,10 +1,15 @@
 package edu.stlawu.stopwatch;
 
+import android.animation.Animator;
+import android.animation.AnimatorInflater;
+import android.media.AudioAttributes;
+import android.media.SoundPool;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -15,9 +20,12 @@ public class MainActivity extends AppCompatActivity {
     private TextView tv_count = null;
     private Button bt_start = null;
     private Button bt_stop =null;
-    private int count = 0;
     private Timer t = null;
     private Counter ctr = null;  // TimerTask
+
+    public AudioAttributes  aa = null;
+    private SoundPool soundPool = null;
+    private int bloopSound = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,28 +40,92 @@ public class MainActivity extends AppCompatActivity {
         this.bt_start.setOnClickListener(new View.OnClickListener() {
              @Override
              public void onClick(View view) {
+                 bt_start.setEnabled(false);
                  t.scheduleAtFixedRate(ctr, 0, 1000);
              }
             });
+
+        this.aa = new AudioAttributes
+                .Builder()
+                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                .setUsage(AudioAttributes.USAGE_GAME)
+                .build();
+
+        this.soundPool = new SoundPool.Builder()
+                .setMaxStreams(1)
+                .setAudioAttributes(aa)
+                .build();
+        this.bloopSound = this.soundPool.load(
+                this, R.raw.bloop, 1);
+
+        this.tv_count.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                soundPool.play(bloopSound, 1f,
+                        1f, 1, 0, 1f);
+                Animator anim = AnimatorInflater
+                        .loadAnimator(MainActivity.this,
+                                       R.animator.counter);
+                anim.setTarget(tv_count);
+                anim.start();
+            }
+        });
+
     }
 
     @Override
     protected void onStart() {
         super.onStart();
 
+        // releoad the count from a previous
+        // run, if first time running, start at 0.
+        /// preferences to share state
+        int count = getPreferences(MODE_PRIVATE)
+                         .getInt("COUNT", 0);
+        this.tv_count.setText(Integer.toString(count));
         this.ctr = new Counter();
+        this.ctr.count = count;
         this.t = new Timer();
 
+        // factory method - design pattern
+        Toast.makeText(this, "Stopwatch is started",
+                        Toast.LENGTH_LONG).show();
 
     }
 
-    class Counter extends TimerTask {
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        getPreferences(MODE_PRIVATE)
+                .edit()
+                .putInt("COUNT", ctr.count)
+                .apply();
+    }
+
+    class Counter extends TimerTask {
+        private int count = 0;
         @Override
         public void run() {
-            MainActivity.this.count++;
-            MainActivity.this.tv_count.setText(
-                    Integer.toString(count));
+            MainActivity.this.runOnUiThread(
+                    new Runnable() {
+                        @Override
+                        public void run() {
+                            MainActivity.this.tv_count.setText(
+                                    Integer.toString(count));
+                            count++;
+                        }
+                    }
+            );
         }
     }
 }
